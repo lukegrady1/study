bd60f3b753ad8deeb53fea7574a8790117f52665
lukegrady1 <lukegrady9@outlook.com>
2025-02-24 15:52:26 -0500

    Update deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
+export default defineConfig(({ command }) => {
   return {
-    base: command === 'build' ? env.VITE_BASE_PATH : '/',
+    base: command === 'build' ? '/study/' : '/',
     plugins: [
       react({ devTarget: 'es2022' }),
     ],
